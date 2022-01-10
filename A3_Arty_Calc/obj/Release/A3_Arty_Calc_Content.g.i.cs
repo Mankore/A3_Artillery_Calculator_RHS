59a3b264aab335d369c2f7b3d6756508a3d50162
Mankor <igorzhelezno@gmail.com>
2022-01-10 16:42:37 +0100

    Add gitignore

--- A3_Arty_Calc/obj/Release/A3_Arty_Calc_Content.g.i.cs
+++ A3_Arty_Calc/obj/Release/A3_Arty_Calc_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("img/sahrani.svg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute(".gitignore")]
 
 
